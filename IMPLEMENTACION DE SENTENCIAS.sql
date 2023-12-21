6084cf3fe2a40c6f9b4b7b38a2ca83c80c7ceb0f
augustomenazzi <augustomenazzi@gmail.com>
2023-12-21 19:21:56 -0300

    SUBLENGUAJE DCL

---
+++ IMPLEMENTACION DE SENTENCIAS.sql
@@ -0,0 +1,14 @@
+-- DESAFIO COMPLEMENTARIO SUBLENGUAJE DCL
+
+USE mysql;
+
+CREATE USER 'usuario1@localhost';
+CREATE USER 'usuario2@localhost';
+
+SHOW GRANTS FOR 'usuario1@localhost';
+SHOW GRANTS FOR 'usuario2@localhost';
+
+
+GRANT SELECT ON *.* TO 'usuario1@localhost';
+
+GRANT SELECT, UPDATE, INSERT ON *.* TO 'usuario2@localhost';
